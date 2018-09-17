91b2ee215a8e3613227c6e6185e5f737058db6af
unknown <876012001@qq.com>
2018-09-17 18:06:14 +0800

    mongoose

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { createStore } from 'redux'
-import './es6'
-class App extends Component {
-  render() {
+import React from 'react'
+class App extends React.Component {
+  render () {
+    const boss = '李云龙'
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to garett</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>独立团,团长{boss}</h2>
+        <一营 老大="张大喵"></一营>
+        <骑兵连 老大="孙德胜"></骑兵连>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+function 骑兵连 (props) {
+  return <h2>骑兵连连长{props.老大}，冲啊！</h2>
+}
+
+class 一营 extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      soliders: ['虎子','柱子','王根生']
+    }
+  }
+  render () {
+    // const boss = '张大喵'
+    return (
+      <div>
+        <h2>一营营长{this.props.老大 }</h2>
+        <ul>
+          {this.state.soliders.map(v=> {
+            return <li key={v}>{v}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+export default App
