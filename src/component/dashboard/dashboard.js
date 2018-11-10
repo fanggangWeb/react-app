38aa31938aa78dab92637926b812f6536bfdb888
fanggang chai <876012001@qq.com>
2018-11-10 22:26:54 +0800

    2018-11-10

--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
+import { connect } from 'react-redux'
+function Boss () {
+  return <h2>Boss首页</h2>
+}
+@connect (
+  state => state
+)
 class Dashboard extends React.Component{
   render () {
+    const user = this.props.user
+    const navList = [
+      {
+        path: '/boss',
+        text: '牛人',
+        icon: 'boss',
+        title: '牛人列表',
+        component: Boss,
+        hide: 
+      }
+    ]
     return (
       <div>
         <h2>header</h2>
-        <Route path="/boss" component={Boss}></Route>
-        <Route path="/genius" component={Genius}></Route>
         <h2>footer</h2>
       </div>
     )
